1e232c9c45bd46e96ff7c855509af53fcb85dc03
techwithdunamix <techwithdunamix@gmail.com>
2024-08-08 13:56:11 +0100

    added page for dashboard

--- frontend/classify/src/components/widgets/pagesnavbar.jsx
+++ frontend/classify/src/components/widgets/pagesnavbar.jsx
@@ -1,6 +1,6 @@
 // src/components/Navbar.js
 import Logo  from './logo';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar bg-black p-4">
@@ -9,7 +9,7 @@ const Navbar = () => {
       </div>
       <div className="flex-none">
         <button className="btn btn-white text-purple-800 transition-transform transform hover:scale-105">
-          Get Started
+          <Link to='/signup'>Signup</Link>
         </button>
       </div>
     </nav>
--- frontend/classify/src/pages/webapp/dashboardhome.jsx
+++ frontend/classify/src/pages/webapp/dashboardhome.jsx
@@ -1,6 +1,67 @@
 import DashboardLayout from "../../components/UI/dashboardlayout";
-
+import { api } from "../../utils";
+import { useState,useEffect } from "react";
+import Loader from "../../components/widgets/loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceMeh ,faPen,faUserFriends} from "@fortawesome/free-solid-svg-icons";
 const DashboardHome = () => {
+    const [classList,setClassList] = useState(null)
+
+    const fetchCLass = () => {
+        api.get("/class",{},5000,
+            (data,status) => {
+                setClassList(data)
+            },
+            (error,status) => {
+                alert("Error")
+            },
+            (error) => {
+                alert("Network error timed out")
+            }
+        )
+    }
+    useEffect(() => {
+        fetchCLass()
+    },[])
+    if (!classList){
+        return (
+            <DashboardLayout>
+                <div className="min-h-[70vh] flex justify-center items-center">
+                    <div>
+                        <Loader />
+                        <p className="mt-5 text-slate-700">Loading your class list</p>
+                    </div>
+
+                </div>
+            </DashboardLayout>
+        )
+    }
+    if (classList.length === 0){
+        return (
+            <DashboardLayout>
+                <div className="min-h-[70vh] flex justify-center items-center">
+                    <div className="text-center">
+                        <FontAwesomeIcon icon={faFaceMeh} className="w-20 h-20 text-purple-300"/>
+                        <p className="mt-5 text-slate-700">Your class list is empty</p>
+                        <div className="flex flex-2 mt-12 justify-between gap-14">
+                            <button className="btn bg-purple-900 px-4 py-2 text-white font-extralight">
+                                <FontAwesomeIcon icon={faUserFriends} />
+                                Join
+                            </button>
+                            <button className="btn bg-white px-4 py-2 border-2 border-purple-900 text-purple-900">
+                            <FontAwesomeIcon icon={faPen} />
+
+                                Create
+                            </button>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </DashboardLayout>
+        )
+    }
     return (
         <DashboardLayout>
             <p>Hello world</p>
