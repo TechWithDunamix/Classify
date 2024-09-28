1da3c4dfee71460df069f1f08923c8be9b23c880
techwithdunamix <techwithdunamix@gmail.com>
2024-09-28 22:46:02 +0100

    add ply fills for mobile

--- frontend/classify/src/components/signupform.jsx
+++ frontend/classify/src/components/signupform.jsx
@@ -105,8 +105,6 @@ const SignUpForm = () => {
       (err, status) => {
 
         SetIsLoading(false)
-        document.querySelector("#root").innerHTML = Object.entries
-        alert(err)
         if (status === 400){
           setStep(1)
           setFormErrors(
